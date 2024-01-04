b13d7fcdf94cc9fe005119b35255e708062e126b
Shashank245 <36560447+Shashank245@users.noreply.github.com>
2024-01-04 22:48:35 +0530

    Navbar

--- digitalmarketplace/src/components/NavItems.tsx
+++ digitalmarketplace/src/components/NavItems.tsx
@@ -1,9 +1,38 @@
-const NavItems = () => {
-    return (
-        <div>
+"use client";
 
-        </div>
-    );
+import { PRODUCT_CATEGORIES } from "@/configs";
+import { useState } from "react";
+import NavItem from "./NavItem";
+
+const NavItems = () => {
+  const [activeIndex, setActiveIndex] = useState<null | number>(null);
+  const isAnyOpen = activeIndex !== null;
+  
+  return (
+    <div className="flex gap-4 h-full">
+      {PRODUCT_CATEGORIES.map((category, i) => {
+        const handleOpen = () => {
+          if (activeIndex === i) {
+            setActiveIndex(null);
+          } else {
+            setActiveIndex(i);
+          }
+        };
+
+        const isOpen = i === activeIndex;
+
+        return (
+          <NavItem
+            key={category.value}
+            category={category}
+            handleOpen={handleOpen}
+            isAnyOpen={isAnyOpen}
+            isOpen={isOpen}
+          />
+        );
+      })}
+    </div>
+  );
 };
 
 export default NavItems;
