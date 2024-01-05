37f566b20cd26665a1183961fcad1139d12c2169
Shashank245 <36560447+Shashank245@users.noreply.github.com>
2024-01-05 21:00:46 +0530

    Fix color

--- digitalmarketplace/src/components/Cart.tsx
+++ digitalmarketplace/src/components/Cart.tsx
@@ -15,7 +15,7 @@ const Cart = () => {
       <SheetTrigger className="group -m-2 flex items-center p-2">
         <ShoppingCartIcon
           aria-hidden="true"
-          className="h-6 w-6 flex-shrink-0 text-gray-400 group-hover:text-red-500"
+          className="h-6 w-6 flex-shrink-0 text-gray-400 group-hover:text-gray-500"
         />
         <span className="ml-2 text-sm font-medium text-gray-700 group-hover:text-gray-800">
           0
