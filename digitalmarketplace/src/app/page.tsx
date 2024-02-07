9f813f53484c11167891485f90b41b766b0e29e4
Shashank245 <36560447+Shashank245@users.noreply.github.com>
2024-02-08 00:16:25 +0530

    Product Reel for displaying Products on our page - IN Progress

--- digitalmarketplace/src/app/page.tsx
+++ digitalmarketplace/src/app/page.tsx
@@ -1,4 +1,5 @@
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
+import ProductReel from "@/components/ProductReel";
 import { Button, buttonVariants } from "@/components/ui/button";
 import { ArrowDownToLine, CheckCircle, Leaf } from "lucide-react";
 import Link from "next/link";
@@ -45,6 +46,7 @@ export default function Home() {
             <Button variant="ghost"> Our Quality Promise &rarr;</Button>
           </div>
         </div>
+        <ProductReel title="Brand New" href="/products" />
       </MaxWidthWrapper>
       <section className="border-t border-gray-200 bg-gray-50">
         <MaxWidthWrapper className="py-20">
