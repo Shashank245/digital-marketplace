e533c0eed830d2c7f6ccf2561732160fb77bc43d
Shashank245 <36560447+Shashank245@users.noreply.github.com>
2024-02-13 07:56:09 +0530

    Checkout page

--- digitalmarketplace/src/app/cart/page.tsx
+++ digitalmarketplace/src/app/cart/page.tsx
@@ -1,16 +1,28 @@
 "use client";
+import { Button } from "@/components/ui/button";
+import { PRODUCT_CATEGORIES } from "@/configs";
 import { useCart } from "@/hooks/use-cart";
-import { cn } from "@/lib/utils";
+import { cn, formatPrice } from "@/lib/utils";
+import { Check, Loader2, X } from "lucide-react";
 import Image from "next/image";
+import Link from "next/link";
 import { useEffect, useState } from "react";
 
 const Page = () => {
   const { items, removeItem } = useCart();
-
+  const fee = 1;
+  const cartTotal = items.reduce(
+    (total, { product }) => total + product.price,
+    0
+  );
   const [isMounted, setIsMounted] = useState<Boolean>(false);
   useEffect(() => {
     setIsMounted(true);
   }, []);
+  function createCheckoutSession(arg0: { productIds: any; }) {
+    throw new Error("Function not implemented.");
+  }
+
   return (
     <div className="bg-white">
       <div className="mx-auto max-w-2xl px-4 pb-24 pt-16 sm:px-6 lg:max-w-7xl lg:px-8">
@@ -44,7 +56,136 @@ const Page = () => {
                 </p>
               </div>
             ) : null}
+            <ul
+              className={cn({
+                "divide-y divide-gray-200 border-b border-t border-gray-200":
+                  isMounted && items.length > 0,
+              })}
+            >
+              {isMounted &&
+                items.map(({ product }) => {
+                  const label = PRODUCT_CATEGORIES.find(
+                    (x) => x.value === product.categories
+                  )?.label;
+                  const { image } = product.images[0];
+                  return (
+                    <li key={product.id} className="flex py-6 sm:py-10">
+                      <div className="flex-shrink-0">
+                        <div className="relative h-24 w-24">
+                          {typeof image != "string" && image.url ? (
+                            <Image
+                              src={image.url}
+                              alt="product category image"
+                              fill
+                              className="object-cover object-center"
+                            />
+                          ) : null}
+                        </div>
+                      </div>
+
+                      <div className="ml-4 flex flex-1 flex-col justify-between sm:ml-6">
+                        <div className="relative pr-9 sm:grid sm:grid-cols-2 sm:gap-x-6 sm:pr-0">
+                          <div>
+                            <div className="flex justify-between">
+                              <h3 className="text-sm">
+                                <Link
+                                  href={`/product/${product.id}`}
+                                  className="font-medium text-gray-700 hover:text-gray-800"
+                                >
+                                  {product.name}
+                                </Link>
+                              </h3>
+                            </div>
+                            <div className="mt-1 flex text-sm">
+                              <p className="text-muted-foreground">
+                                Category: {label}
+                              </p>
+                            </div>
+
+                            <p className="mt-1 text-sm font-medium text-gray-900">
+                              {" "}
+                              {formatPrice(product.price)}{" "}
+                            </p>
+                          </div>
+                          <div className="mt-4 sm:mt-0 sm:pr-9 w-20">
+                            <div className="absolute right-0 top-0">
+                              <Button
+                                aria-label="remove product"
+                                variant="ghost"
+                                onClick={() => removeItem(product.id)}
+                              >
+                                <X className="h-5 w-5" aria-hidden="true" />
+                              </Button>
+                            </div>
+                          </div>
+                        </div>
+                        <p className="mt-6 flex space-x-2 text-sm text-gray-700">
+                          <Check className="h-5 w-5 flex-shrink-0 text-green-500" />
+
+                          <span>Eligible for instant delivery</span>
+                        </p>
+                      </div>
+                    </li>
+                  );
+                })}
+            </ul>
           </div>
+          <section className="mt-16 rounded-lg px-4 py-6 bg-gray-50 sm:p-6 lg:col-span-5 lg:mt-0 lg:p-8'">
+            <h2 className="text-lg font-medium text-gray-900">
+              {" "}
+              Order Summary
+            </h2>
+            <div className="mt-6 space-y-4">
+              <div className="flex items-center justify-between">
+                <p className="text-sm text-gray-600">Subtotal</p>
+                <p className="text-sm font-medium text-gray-900">
+                  {isMounted ? (
+                    formatPrice(cartTotal)
+                  ) : (
+                    <Loader2 className="h-4 w-4 animate-spin text-muted-foreground" />
+                  )}
+                </p>
+              </div>
+              <div className="flex items-center justify-between border-t border-gray-200 pt-4">
+                <div className="flex items-center text-sm text-muted-foreground">
+                  <span>Flat Transaction Fee</span>
+                </div>
+                <div className="text-sm font-medium text-gray-900">
+                  {isMounted ? (
+                    formatPrice(fee)
+                  ) : (
+                    <Loader2 className="h-4 w-4 animate-spin text-muted-foreground" />
+                  )}
+                </div>
+              </div>
+              <div className="flex items-center justify-between border-t border-gray-200 pt-4">
+                <div className="text-base font-medium text-gray-900">
+                  Order Total
+                </div>{" "}
+                <div className="text-base font-medium text-gray-900">
+                  {isMounted ? (
+                    formatPrice(cartTotal + fee)
+                  ) : (
+                    <Loader2 className="h-4 w-4 animate-spin text-muted-foreground" />
+                  )}
+                </div>
+              </div>{" "}
+            </div>
+            <div className="mt-6">
+              <Button
+                className="w-full"
+                onClick={() => {
+                  createCheckoutSession({ productIds });
+                }}
+                size="lg"
+              >
+                {isLoading ? (
+                  <Loader2 className="w-4 h-4 animate-spin mr-1.5" />
+                ) : null}
+                Checkout
+              </Button>
+            </div>
+          </section>
         </div>
       </div>
     </div>
