501d495845099b1c7e93bbd5ce4ba100c2e16000
Shashank245 <36560447+Shashank245@users.noreply.github.com>
2024-02-21 02:37:21 +0530

    Complete Thank you page

--- digitalmarketplace/src/app/thank-you/page.tsx
+++ digitalmarketplace/src/app/thank-you/page.tsx
@@ -1,7 +1,11 @@
+import { PRODUCT_CATEGORIES } from "@/configs";
 import { getPayloadClient } from "@/get-payload";
 import { getServerSideUser } from "@/lib/payload-utils";
+import { formatPrice } from "@/lib/utils";
+import { Product, ProductFile } from "@/payload-types";
 import { cookies } from "next/headers";
 import Image from "next/image";
+import Link from "next/link";
 import { notFound, redirect } from "next/navigation";
 
 interface PageProps {
@@ -23,6 +27,7 @@ const ThankyouPage = async ({ searchParams }: PageProps) => {
       },
     },
   });
+
   const [order] = orders;
 
   if (!order) return notFound();
@@ -31,8 +36,14 @@ const ThankyouPage = async ({ searchParams }: PageProps) => {
     typeof order.user === "string" ? order.user : order.user.id;
 
   if (orderUserId !== user?.id) {
-    return redirect(`/sign-in?origin=thank-you?orderId=${orderId}`);
+    return redirect(`/sign-in?origin=thank-you?orderid=${orderId}`);
   }
+
+  const products = order.products as Product[];
+
+  const orderTotal = products.reduce((total, product) => {
+    return total + product.price;
+  }, 0);
 
   return (
     <main className="relative lg:min-h-full">
@@ -73,6 +84,77 @@ const ThankyouPage = async ({ searchParams }: PageProps) => {
                 soon!
               </p>
             )}
+            <div className="mt-16 text-sm font-medium">
+              <div className="text-muted-foreground">Order nr.</div>
+              <div className="mt-2 text-gray-900">{order.id}</div>
+              <ul className="mt-6 divide-y divide-gray-200 border-t border-gray-200 text-sm font-medium text-muted-foreground">
+                {(order.products as Product[]).map((product) => {
+                  const label = PRODUCT_CATEGORIES.find(
+                    ({ value }) => value === product.categories
+                  )?.label;
+
+                  const downloadUrl = (product.product_files as ProductFile)
+                    .url as string;
+
+                  const { image } = product.images[0];
+                  return (
+                    <li key={product.id} className="flex space-x-6 py-6">
+                      <div className="relative h-24 w-24">
+                        {typeof image !== "string" && image.url ? (
+                          <Image
+                            fill
+                            src={image.url}
+                            alt={`${product.name} image`}
+                            className="flex-none rounded-md bg-gray-100 object-cover object-center"
+                          />
+                        ) : null}
+                      </div>
+                      <div className="flex-auto flex flex-col justify-between">
+                        <div className="space-y-1">
+                          <h3 className="text-gray-900">{product.name}</h3>
+                          <p className="my-1">Category: {label}</p>
+                        </div>
+                        {order._isPaid ? (
+                          <a
+                            href={downloadUrl}
+                            download={product.name}
+                            className="text-blue-600 hover:underline underline-offset-2"
+                          >
+                            Download asset
+                          </a>
+                        ) : null}
+                      </div>
+                      <p className="flex font-medium text-gray-900">
+                        {formatPrice(orderTotal)}
+                      </p>
+                    </li>
+                  );
+                })}
+              </ul>
+              <div className="space-y-6 border-t border-gray-200 pt-6 text-sm font-medium text-muted-foreground">
+                <div className="flex justify-between">
+                  <p>Subtotal</p>
+                  <p>{formatPrice(orderTotal)}</p>
+                </div>
+                <div className="flex justify-between">
+                  <p>Transaction Fee</p>
+                  <p className="text-gray-900">{formatPrice(1)}</p>
+                </div>
+                <div className="flex items-center justify-between border-t border-gray-200 pt-6 text-gray-900">
+                  <p className="text-base">Total</p>
+                  <p className="text-base">{formatPrice(orderTotal + 1)}</p>
+                </div>
+              </div>
+
+              <div className="mt-16 border-t border-gray-200 py-6 text-right">
+                <Link
+                  href="/products"
+                  className="text-sm font-medium text-blue-600 hover:text-blue-500"
+                >
+                  Continue shopping &rarr;
+                </Link>
+              </div>
+            </div>
           </div>
         </div>
       </div>
