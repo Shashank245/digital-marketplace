83a3f8d1fc27466b16a1190c9e7acc5496341066
Shashank245 <36560447+Shashank245@users.noreply.github.com>
2024-02-21 01:30:31 +0530

    commit changes related to thank youpage

--- digitalmarketplace/src/app/thank-you/page.tsx
+++ digitalmarketplace/src/app/thank-you/page.tsx
@@ -1,11 +1,39 @@
+import { getPayloadClient } from "@/get-payload";
+import { getServerSideUser } from "@/lib/payload-utils";
+import { cookies } from "next/headers";
 import Image from "next/image";
+import { notFound, redirect } from "next/navigation";
 
 interface PageProps {
-    searchParams: {
-        [key: string]: string | string[] | undefined
-    }
+  searchParams: {
+    [key: string]: string | string[] | undefined;
+  };
 }
-const ThankyouPage = ({ searchParams }: PageProps) => {
+const ThankyouPage = async ({ searchParams }: PageProps) => {
+  const orderId = searchParams.orderId;
+  const nextCookies = cookies();
+  const { user } = await getServerSideUser(nextCookies);
+  const payload = await getPayloadClient();
+  const { docs: orders } = await payload.find({
+    collection: "orders",
+    depth: 2,
+    where: {
+      id: {
+        equals: orderId,
+      },
+    },
+  });
+  const [order] = orders;
+
+  if (!order) return notFound();
+
+  const orderUserId =
+    typeof order.user === "string" ? order.user : order.user.id;
+
+  if (orderUserId !== user?.id) {
+    return redirect(`/sign-in?origin=thank-you?orderId=${orderId}`);
+  }
+
   return (
     <main className="relative lg:min-h-full">
       <div className="hidden lg:block h-80 overflow-hidden lg:absolute lg:h-full lg:w-1/2 lg:pr-4 xl:pr-12">
@@ -26,6 +54,25 @@ const ThankyouPage = ({ searchParams }: PageProps) => {
             <h1 className="mt-2 text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl">
               Thanks for ordering
             </h1>
+            {order._isPaid ? (
+              <p className="mt-2 text-base text-muted-foreground">
+                Your order was processed and your assets are available to
+                download below. We&apos;ve sent your receipt and order details
+                to{" "}
+                {typeof order.user !== "string" ? (
+                  <span className="font-medium text-gray-900">
+                    {order.user.email}
+                  </span>
+                ) : null}
+                .
+              </p>
+            ) : (
+              <p className="mt-2 text-base text-muted-foreground">
+                We appreciate your order, and we&apos;re currently processing
+                it. So hang tight and we&apos;ll send you confirmation very
+                soon!
+              </p>
+            )}
           </div>
         </div>
       </div>
