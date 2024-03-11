b9beef08c37ff718ea4391dbcf67f7d4bff3407b
Shashank245 <36560447+Shashank245@users.noreply.github.com>
2024-03-11 20:13:38 +0530

    commit static path

--- digitalmarketplace/src/components/ImageSlider.tsx
+++ digitalmarketplace/src/components/ImageSlider.tsx
@@ -85,7 +85,7 @@ const ImageSlider = ({ urls }: ImageSlierProps) => {
           {urls.map((url, i) => (
             <SwiperSlide key={i} className="-z-10 w-full h-full relative">
               <Image
-                src={url}
+                src='/checkout-thank-you.jpg'
                 alt=""
                 fill
                 loading="eager"
