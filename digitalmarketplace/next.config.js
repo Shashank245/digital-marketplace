e6d6ee67f0877b0fc448f771551af8f9626c06de
Shashank245 <36560447+Shashank245@users.noreply.github.com>
2024-03-11 09:43:02 +0530

    commit

--- digitalmarketplace/next.config.js
+++ digitalmarketplace/next.config.js
@@ -1,13 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "digital-marketplace-production-dc8f.up.railway.app",
-        port: "",
-      },
-    ],
+    domains: ["digital-marketplace-production-dc8f.up.railway.app"],
   },
 };
 
